b6adaae978514ba08815cb184b8c4ef84096e7b9
tomasmax <critimass@gmail.com>
2015-02-03 17:55:10 +0100

    UILabs ToDoManagerActivity done

--- UserInterface/SourceFiles/Skeletons/UILabs/src/course/labs/todomanager/ToDoManagerActivity.java
+++ UserInterface/SourceFiles/Skeletons/UILabs/src/course/labs/todomanager/ToDoManagerActivity.java
@@ -88,6 +88,10 @@ public class ToDoManagerActivity extends ListActivity {
 		// if user submitted a new ToDoItem
 		// Create a new ToDoItem from the data Intent
 		// and then add it to the adapter
+		if (requestCode == ADD_TODO_ITEM_REQUEST && resultCode == RESULT_OK) {
+			ToDoItem toDoItem = new ToDoItem(data);
+			mAdapter.add(toDoItem);
+		}
 
 	}
 
